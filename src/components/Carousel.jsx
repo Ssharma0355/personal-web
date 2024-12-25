67ecf9a0293211d4c946f19774097997f6df6ee7
sachin_sde <ssachin.s@myclnq.co.in>
2024-12-25 17:18:06 +0530

    sejal

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,8 +6,9 @@ import slide2 from '../assets/slide2.png';
 import slide3 from '../assets/slide3.png';
 import slide4 from '../assets/slide4.png';
 import slide5 from '../assets/slide5.png';
+import slide6 from '../assets/slide6.png';
 
-const slides = [slide1, slide2, slide3, slide4, slide5];
+const slides = [slide1, slide2, slide3, slide4, slide5, slide6];
 
 const Carousel = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
